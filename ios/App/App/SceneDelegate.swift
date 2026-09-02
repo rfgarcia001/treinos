d31ad3346b5c27053d5ca87f32c3266b502fbbda
rfgarcia001 <104100358+rfgarcia001@users.noreply.github.com>
2026-09-02 10:37:51 -0300

    refactor(ios): use native web view

--- ios/App/App/SceneDelegate.swift
+++ ios/App/App/SceneDelegate.swift
@@ -1,5 +1,4 @@
 import UIKit
-import Capacitor
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     var window: UIWindow?
@@ -8,17 +7,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let windowScene = scene as? UIWindowScene else { return }
 
         window = UIWindow(windowScene: windowScene)
-        window?.rootViewController = CAPBridgeViewController()
+        window?.rootViewController = WorkoutViewController()
         window?.makeKeyAndVisible()
-
-        SceneDelegateProxy.shared.scene(scene, willConnectTo: session, options: connectionOptions)
-    }
-
-    func scene(_ scene: UIScene, openURLContexts URLContexts: Set<UIOpenURLContext>) {
-        SceneDelegateProxy.shared.scene(scene, openURLContexts: URLContexts)
-    }
-
-    func scene(_ scene: UIScene, continue userActivity: NSUserActivity) {
-        SceneDelegateProxy.shared.scene(scene, continue: userActivity)
     }
 }
